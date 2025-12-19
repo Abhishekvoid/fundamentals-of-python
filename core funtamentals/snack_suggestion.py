ac17b7e6437d5cc13a6e59bf0d965466c4d919f6
abhishekvoid <ab14609a@gmail.com>
2025-12-19 23:21:30 +0530

    if-else snack suggestion

---
+++ core funtamentals/snack_suggestion.py
@@ -0,0 +1,7 @@
+
+snack = input("Enter your preferred snack: ").lower()
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great choice! We'll serve you {snack}")
+else:
+    print(f"sorry, we only serve cookies or samosa with tea")
